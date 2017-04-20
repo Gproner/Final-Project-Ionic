ebf292276edd253574dffd5091b665f4784529a8
Simon Mc Neil <mcneils@sheridancollege.ca>
2017-04-19 22:51:49 -0400

    database works
    Implemented storing of user info in database

---
+++ www/js/controllers.js
@@ -0,0 +1,44 @@
+angular.module('app.controllers', [])
+  
+.controller('graphCtrl', ['$scope', '$stateParams', 
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('flashCardsCtrl', ['$scope', '$stateParams',
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('settingsCtrl', ['$scope', '$stateParams',
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams',
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams',
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams',
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('quizCreatorCtrl', ['$scope', '$stateParams',
+function ($scope, $stateParams) {
+
+
+}])
+ 

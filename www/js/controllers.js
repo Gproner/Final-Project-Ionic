618f9801ba23c8f59e6c44ed36549c30c744313c
george <gproner@gmail.com>
2017-04-21 10:58:36 -0400

    added graph
    added graph

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,31 @@ angular.module('app.controllers', ['ionic','ngCordova'])
   
 .controller('graphCtrl', ['$scope', '$stateParams', 
 function ($scope, $stateParams) {
+   function draw() {
+    try {
+      functionPlot({
+        target: '#plot',
+        data: [{
+          fn: document.getElementById('eq').value,
+          sampler: 'builtIn',  // this will make function-plot use the evaluator of math.js
+          graphType: 'polyline'
+        }]
+      });
+    }
+    catch (err) {
+      console.log(err);
+      alert(err);
+    }
+  }
+
+  document.getElementById('form').onsubmit = function (event) {
+    event.preventDefault();
+    draw();
+  };
+
+  draw();
+
+
 
 
 }])
